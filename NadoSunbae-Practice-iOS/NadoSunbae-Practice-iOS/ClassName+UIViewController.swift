1fa0c7c2a4ff03189ce41056bf1bffc85fdccbfd
eunjuchoi <eunjuzzoo@naver.com>
2022-01-05 19:52:20 +0900

    ➕[ADD] : extension 파일 추가

---
+++ NadoSunbae-Practice-iOS/NadoSunbae-Practice-iOS/ClassName+UIViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClassName+UIViewController.swift
+//  NadoSunbae-Practice-iOS
+//
+//  Created by EUNJU on 2022/01/05.
+//
+
+import UIKit
+
+extension UIViewController {
+    static var className: String {
+        NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
+    }
+    
+     var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+}
+
+extension UIView {
+    static var className: String {
+        NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
+    }
+    
+     var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+}
+
---
+++ NadoSunbae-Practice-iOS/NadoSunbae-Practice-iOS/UIViewController+.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+.swift
+//  NadoSunbae-Practice-iOS
+//
+//  Created by EUNJU on 2022/01/05.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
