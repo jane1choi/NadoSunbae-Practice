8bd6410c5ada0884a81c3e5f7001cdd4143e4737
eunjuchoi <eunjuzzoo@naver.com>
2022-01-06 17:55:20 +0900

    ✨[FEAT] : dismiss 방식 슬라이드에서 버튼클릭으로 변경

--- NadoSunbae-Practice-iOS/NadoSunbae-Practice-iOS/Views/OverlayView.swift
+++ NadoSunbae-Practice-iOS/NadoSunbae-Practice-iOS/Views/OverlayView.swift
@@ -14,8 +14,8 @@ class OverlayView: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerAction))
-        view.addGestureRecognizer(panGesture)
+//        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerAction))
+//        view.addGestureRecognizer(panGesture)
         
     }
     
@@ -25,26 +25,30 @@ class OverlayView: UIViewController {
             pointOrigin = self.view.frame.origin
         }
     }
-    @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
-        let translation = sender.translation(in: view)
+    @IBAction func didTapDismissBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
         
-        // Not allowing the user to drag the view upward
-        guard translation.y >= 0 else { return }
-        
-        // setting x as 0 because we don't want users to move the frame side ways!! Only want straight up or down
-        view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + translation.y)
-        
-        if sender.state == .ended {
-            let dragVelocity = sender.velocity(in: view)
-            if dragVelocity.y >= 1300 {
-                self.dismiss(animated: true, completion: nil)
-            } else {
-                // Set back to original position of the view controller
-                UIView.animate(withDuration: 0.3) {
-                    self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: 400)
-                }
-            }
-        }
     }
+    //    @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
+//        let translation = sender.translation(in: view)
+//        
+//        // Not allowing the user to drag the view upward
+//        guard translation.y >= 0 else { return }
+//        
+//        // setting x as 0 because we don't want users to move the frame side ways!! Only want straight up or down
+//        view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + translation.y)
+//        
+//        if sender.state == .ended {
+//            let dragVelocity = sender.velocity(in: view)
+//            if dragVelocity.y >= 1300 {
+//                self.dismiss(animated: true, completion: nil)
+//            } else {
+//                // Set back to original position of the view controller
+//                UIView.animate(withDuration: 0.3) {
+//                    self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: 400)
+//                }
+//            }
+//        }
+//    }
     
 }
