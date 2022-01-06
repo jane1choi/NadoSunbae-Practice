1d00ce85e091eab34441866005e6f286b27c3c70
eunjuchoi <eunjuzzoo@naver.com>
2022-01-06 17:47:11 +0900

    - ✨[FEAT] : 바텀시트 구현

---
+++ NadoSunbae-Practice-iOS/NadoSunbae-Practice-iOS/Models/MajorModel.swift
@@ -0,0 +1,8 @@
+//
+//  MajorModel.swift
+//  NadoSunbae-Practice-iOS
+//
+//  Created by EUNJU on 2022/01/06.
+//
+
+import Foundation
---
+++ NadoSunbae-Practice-iOS/NadoSunbae-Practice-iOS/Views/OverlayView.swift
@@ -0,0 +1,50 @@
+//
+//  OverlayView.swift
+//  NadoSunbae-Practice-iOS
+//
+//  Created by EUNJU on 2022/01/06.
+//
+
+import UIKit
+
+class OverlayView: UIViewController {
+    
+    var hasSetPointOrigin = false
+    var pointOrigin: CGPoint?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerAction))
+        view.addGestureRecognizer(panGesture)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        if !hasSetPointOrigin {
+            hasSetPointOrigin = true
+            pointOrigin = self.view.frame.origin
+        }
+    }
+    @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        // Not allowing the user to drag the view upward
+        guard translation.y >= 0 else { return }
+        
+        // setting x as 0 because we don't want users to move the frame side ways!! Only want straight up or down
+        view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + translation.y)
+        
+        if sender.state == .ended {
+            let dragVelocity = sender.velocity(in: view)
+            if dragVelocity.y >= 1300 {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                // Set back to original position of the view controller
+                UIView.animate(withDuration: 0.3) {
+                    self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: 400)
+                }
+            }
+        }
+    }
+    
+}
