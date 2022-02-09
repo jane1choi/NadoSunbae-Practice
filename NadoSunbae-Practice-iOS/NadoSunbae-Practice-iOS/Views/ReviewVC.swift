f506ce848bb1d0c1525f64ccbe4d6d680058b5b0
eunjuchoi <eunjuzzoo@naver.com>
2022-02-09 16:40:30 +0900

    ⚰️[DELETE] : 쓸모없는 코드 삭제 및 코드 정리

--- NadoSunbae-Practice-iOS/NadoSunbae-Practice-iOS/Views/ReviewVC.swift
+++ NadoSunbae-Practice-iOS/NadoSunbae-Practice-iOS/Views/ReviewVC.swift
@@ -11,8 +11,6 @@ class ReviewVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @objc func showHalfView() {
